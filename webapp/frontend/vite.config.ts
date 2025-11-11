37069692d0e192602dd93d676661e2758e295479
Aditya Miriyala <adityamiriyala@gmail.com>
2025-11-10 22:19:51 -0600

    Update project files: fix frontend proxy and other changes

--- webapp/frontend/vite.config.ts
+++ webapp/frontend/vite.config.ts
@@ -5,18 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: 'localhost',
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:8000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/ws': {
-        target: 'ws://127.0.0.1:8000',
-        ws: true,
-        changeOrigin: true,
-      },
+        '/api': {
+          target: 'http://127.0.0.1:8000',
+          changeOrigin: true,
+          secure: false,
+        },
+        '/ws': {
+          target: 'ws://127.0.0.1:8000',
+          ws: true,
+          changeOrigin: true,
+        },
     },
   },
   build: {
